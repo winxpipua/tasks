d5d27fbb81f7022da3b8bb69e816353d7feb264b
Alexander <eas@iteora.com>
2015-10-15 18:23:05 +0300

    add Yii framework

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic_tasks',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
